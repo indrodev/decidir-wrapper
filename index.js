6ee7f92dca0b314ded7ecf8ad244d5e631a265f5
Indrajit Roy <developerindrajit@gmail.com>
2020-03-12 13:45:49 +0530

    Few text changes

--- index.js
+++ index.js
@@ -1,96 +1,94 @@
+/* eslint-disable dot-notation */
+/* eslint-disable object-shorthand */
 const axios = require("axios")
 
 const baseUrls = {
-  "development": "https://developers.decidir.com/api/v2",
-  "production": "https://live.decidir.com/api/v2"
+  development: "https://developers.decidir.com/api/v2",
+  production: "https://live.decidir.com/api/v2"
 }
 
 const endpoints = {
   healthCheck: "/healthcheck",
   removeCardToken: "/cardtokens",
   cardList: {
-    "0": "/usersite",
-    "1": "/cardtokens"
+    0: "/usersite",
+    1: "/cardtokens"
   },
   paymentsList: "/payments",
   makePayment: "/payments",
   paymentRefund: {
-    "0": "/payments",
-    "1": "/refunds",
+    0: "/payments",
+    1: "/refunds",
   }
 }
 
 module.exports = {
-  sdk: function ({ mode = "development", privateKey, publicKey, globalSiteId, globalSiteName }) {
+  sdk: function ({
+    mode = "development", privateKey, publicKey, globalSiteId, globalSiteName
+  }) {
     if (!(mode && privateKey)) throw new Error("Missing private key")
-    
+
     axios.defaults.baseURL = baseUrls[mode]
-    
+
     const headers = {
       apiKey: privateKey,
       "Content-Type": "application/json",
       "Cache-Control": "no-cache"
     }
-    
+
     /**
     * Check payment gateway health
     * @return {Promise}
     */
-    this.healthCheck = () => {
-      return new Promise((resolve, reject) => {
-        axios({
-          method: "get",
-          url: endpoints["healthCheck"],
-        }).then((response) => {
-          resolve(response.data)
-        }).catch((err) => {
-          reject(err)
-        })
+    this.healthCheck = () => new Promise((resolve, reject) => {
+      axios({
+        method: "get",
+        url: endpoints["healthCheck"],
+      }).then((response) => {
+        resolve(response.data)
+      }).catch((err) => {
+        reject(err)
       })
-    }
+    })
     /**
     * Customer remove card
     * @param {string} cardToken - Customer's cardToken
     * @return {Promise}
     */
-    this.removeCustomerCard = (cardToken) => {
-      return new Promise((resolve, reject) => {
-        if (!(cardToken && typeof cardToken === "string")) {
-          reject(new Error("Invalid card token"))
-        }
-        axios({
-          method: "delete",
-          url: `${endpoints["removeCardToken"]}/${cardToken}`,
-          headers
-        }).then((response) => {
-          resolve(response.data)
-        }).catch((err) => {
-          reject(err)
-        })
+    this.removeCustomerCard = cardToken => new Promise((resolve, reject) => {
+      if (!(cardToken && typeof cardToken === "string")) {
+        reject(new Error("Invalid card token"))
+      }
+      axios({
+        method: "delete",
+        url: `${endpoints["removeCardToken"]}/${cardToken}`,
+        headers
+      }).then((response) => {
+        resolve(response.data)
+      }).catch((err) => {
+        reject(err)
       })
-    }
-    
+    })
+
     /**
     * Customer card list
     * @param {string} userId - Customer's id
     * @return {Promise}
     */
-    this.getCustomerCardList = (userId) => {
-      return new Promise((resolve, reject) => {
-        if (!(userId && typeof userId === "string")) {
-          reject(new Error("Invalid user id"))
-        }
-        axios({
-          method: "get",
-          url: `${endpoints["cardList"]["0"]}/${userId}${endpoints["cardList"]["1"]}`,
-          headers
-        }).then((response) => {
-          resolve(response.data)
-        }).catch((err) => {
-          reject(err)
-        })
+    this.getCustomerCardList = userId => new Promise((resolve, reject) => {
+      if (!(userId && typeof userId === "string")) {
+        reject(new Error("Invalid user id"))
+      }
+      axios({
+        method: "get",
+        url: `${endpoints["cardList"]["0"]}/${userId}${endpoints["cardList"]["1"]}`,
+        headers
+      }).then((response) => {
+        resolve(response.data)
+      }).catch((err) => {
+        reject(err)
       })
-    } 
+    })
     /**
     * Make split payment with a single vendor
     * @param {object} capturePayment - The payment object
@@ -108,99 +106,97 @@ module.exports = {
     * @param {string} [capturePayment.description=null] - Description of the transaction.
     * @return {Promise}
     */
-    this.singleSplitPayment = (capturePayment) => {
-      return new Promise((resolve, reject) => {
-        const {
-          siteTransactionId,
-          cardToken,
-          userId,
-          email = null,
-          paymentMethodId,
-          bin,
-          amount,
-          currency = "ARS",
-          installments = 1,
-          description = null,
-          vendorId,
-          vendorAmount
-        } = capturePayment
-        if (!(siteTransactionId && typeof siteTransactionId === "string")) {
-          reject(new Error("Invalid siteTransactionId"))
-        }
-        if (!(cardToken && typeof cardToken === "string")) {
-          reject(new Error("Invalid cardToken"))
-        }
-        if (!(userId && typeof userId === "string")) {
-          reject(new Error("Invalid userId"))
-        }
-        if (!(paymentMethodId && typeof paymentMethodId === "number")) {
-          reject(new Error("Invalid paymentMethodId"))
-        }
-        if (!(bin && typeof bin === "string")) {
-          reject(new Error("Invalid bin"))
-        }
-        if (!(amount && typeof amount === "number" && amount > 0)) {
-          reject(new Error("Invalid amount"))
-        }
-        if (!(currency && typeof currency === "string")) {
-          reject(new Error("Invalid currency"))
-        }
-        if (!(currency && typeof currency === "string")) {
-          reject(new Error("Invalid currency"))
-        }
-        if (!(installments && typeof installments === "number")) {
-          reject(new Error("Invalid installments"))
-        }
-        if (!(vendorId && typeof vendorId === "string")) {
-          reject(new Error("Invalid vendorId"))
-        }
-        if (!(vendorAmount && typeof vendorAmount === "number" && vendorAmount > 0)) {
-          reject(new Error("Invalid vendorAmount"))
-        }
-        
-        const payload = {
-          customer: {
-            id: userId,
-            email
-          },
-          site_transaction_id: siteTransactionId,
-          token: cardToken,
-          payment_method_id: paymentMethodId,
-          bin,
-          amount,
-          currency,
-          installments,
-          description,
-          payment_type: "distributed",
-          establishment_name: globalSiteName,
-          sub_payments: []
-        }
-        let platformAmount = Number((amount - vendorAmount).toFixed(2))
-        // Add platform charge
-        payload.sub_payments.push({
-          site_id: globalSiteId,
-          installments: 1,
-          amount: platformAmount
-        })
+    this.singleSplitPayment = capturePayment => new Promise((resolve, reject) => {
+      const {
+        siteTransactionId,
+        cardToken,
+        userId,
+        email = null,
+        paymentMethodId,
+        bin,
+        amount,
+        currency = "ARS",
+        installments = 1,
+        description = null,
+        vendorId,
+        vendorAmount
+      } = capturePayment
+      if (!(siteTransactionId && typeof siteTransactionId === "string")) {
+        reject(new Error("Invalid siteTransactionId"))
+      }
+      if (!(cardToken && typeof cardToken === "string")) {
+        reject(new Error("Invalid cardToken"))
+      }
+      if (!(userId && typeof userId === "string")) {
+        reject(new Error("Invalid userId"))
+      }
+      if (!(paymentMethodId && typeof paymentMethodId === "number")) {
+        reject(new Error("Invalid paymentMethodId"))
+      }
+      if (!(bin && typeof bin === "string")) {
+        reject(new Error("Invalid bin"))
+      }
+      if (!(amount && typeof amount === "number" && amount > 0)) {
+        reject(new Error("Invalid amount"))
+      }
+      if (!(currency && typeof currency === "string")) {
+        reject(new Error("Invalid currency"))
+      }
+      if (!(currency && typeof currency === "string")) {
+        reject(new Error("Invalid currency"))
+      }
+      if (!(installments && typeof installments === "number")) {
+        reject(new Error("Invalid installments"))
+      }
+      if (!(vendorId && typeof vendorId === "string")) {
+        reject(new Error("Invalid vendorId"))
+      }
+      if (!(vendorAmount && typeof vendorAmount === "number" && vendorAmount > 0)) {
+        reject(new Error("Invalid vendorAmount"))
+      }
 
-        // Add vendor charge
-        payload.sub_payments.push({
-          site_id: vendorId,
-          installments: 1,
-          amount: vendorAmount
-        })
-        axios({
-          method: "post",
-          url: `${endpoints["makePayment"]}`,
-          data: payload,
-          headers
-        }).then((response) => {
-          resolve(response.data)
-        }).catch((err) => {
-          reject(err)
-        })
+      const payload = {
+        customer: {
+          id: userId,
+          email
+        },
+        site_transaction_id: siteTransactionId,
+        token: cardToken,
+        payment_method_id: paymentMethodId,
+        bin,
+        amount,
+        currency,
+        installments,
+        description,
+        payment_type: "distributed",
+        establishment_name: globalSiteName,
+        sub_payments: []
+      }
+      const platformAmount = Number((amount - vendorAmount).toFixed(2))
+      // Add platform charge
+      payload.sub_payments.push({
+        site_id: globalSiteId,
+        installments: 1,
+        amount: platformAmount
       })
-    }
+
+      // Add vendor charge
+      payload.sub_payments.push({
+        site_id: vendorId,
+        installments: 1,
+        amount: vendorAmount
+      })
+      axios({
+        method: "post",
+        url: `${endpoints["makePayment"]}`,
+        data: payload,
+        headers
+      }).then((response) => {
+        resolve(response.data)
+      }).catch((err) => {
+        reject(err)
+      })
+    })
     /**
     * Payment without split
     * @param {object} capturePayment - The payment object
@@ -216,134 +212,128 @@ module.exports = {
     * @param {string} [capturePayment.description=null] - Description of the transaction.
     * @return {Promise}
     */
-    this.payment = (capturePayment) => {
-      return new Promise((resolve, reject) => {
-        const {
-          siteTransactionId,
-          cardToken,
-          userId,
-          email = null,
-          paymentMethodId,
-          bin,
-          amount,
-          currency = "ARS",
-          installments = 1,
-          description = null
-        } = capturePayment
-        if (!(siteTransactionId && typeof siteTransactionId === "string")) {
-          reject(new Error("Invalid siteTransactionId"))
-        }
-        if (!(cardToken && typeof cardToken === "string")) {
-          reject(new Error("Invalid cardToken"))
-        }
-        if (!(userId && typeof userId === "string")) {
-          reject(new Error("Invalid userId"))
-        }
-        if (!(paymentMethodId && typeof paymentMethodId === "number")) {
-          reject(new Error("Invalid paymentMethodId"))
-        }
-        if (!(bin && typeof bin === "string")) {
-          reject(new Error("Invalid bin"))
-        }
-        if (!(amount && typeof amount === "number" && amount > 0)) {
-          reject(new Error("Invalid amount"))
-        }
-        if (!(currency && typeof currency === "string")) {
-          reject(new Error("Invalid currency"))
-        }
-        if (!(currency && typeof currency === "string")) {
-          reject(new Error("Invalid currency"))
-        }
-        if (!(installments && typeof installments === "number")) {
-          reject(new Error("Invalid installments"))
-        }
-        
-        const payload = {
-          customer: {
-            id: userId,
-            email
-          },
-          site_transaction_id: siteTransactionId,
-          token: cardToken,
-          payment_method_id: paymentMethodId,
-          bin,
-          amount,
-          currency,
-          installments,
-          description,
-          payment_type: "single",
-          establishment_name: globalSiteName,
-          sub_payments: []
-        }
-        
-        axios({
-          method: "post",
-          url: `${endpoints["makePayment"]}`,
-          data: payload,
-          headers
-        }).then((response) => {
-          resolve(response.data)
-        }).catch((err) => {
-          reject(err)
-        })
+    this.payment = capturePayment => new Promise((resolve, reject) => {
+      const {
+        siteTransactionId,
+        cardToken,
+        userId,
+        email = null,
+        paymentMethodId,
+        bin,
+        amount,
+        currency = "ARS",
+        installments = 1,
+        description = null
+      } = capturePayment
+      if (!(siteTransactionId && typeof siteTransactionId === "string")) {
+        reject(new Error("Invalid siteTransactionId"))
+      }
+      if (!(cardToken && typeof cardToken === "string")) {
+        reject(new Error("Invalid cardToken"))
+      }
+      if (!(userId && typeof userId === "string")) {
+        reject(new Error("Invalid userId"))
+      }
+      if (!(paymentMethodId && typeof paymentMethodId === "number")) {
+        reject(new Error("Invalid paymentMethodId"))
+      }
+      if (!(bin && typeof bin === "string")) {
+        reject(new Error("Invalid bin"))
+      }
+      if (!(amount && typeof amount === "number" && amount > 0)) {
+        reject(new Error("Invalid amount"))
+      }
+      if (!(currency && typeof currency === "string")) {
+        reject(new Error("Invalid currency"))
+      }
+      if (!(currency && typeof currency === "string")) {
+        reject(new Error("Invalid currency"))
+      }
+      if (!(installments && typeof installments === "number")) {
+        reject(new Error("Invalid installments"))
+      }
+
+      const payload = {
+        customer: {
+          id: userId,
+          email
+        },
+        site_transaction_id: siteTransactionId,
+        token: cardToken,
+        payment_method_id: paymentMethodId,
+        bin,
+        amount,
+        currency,
+        installments,
+        description,
+        payment_type: "single",
+        establishment_name: globalSiteName,
+        sub_payments: []
+      }
+
+      axios({
+        method: "post",
+        url: `${endpoints["makePayment"]}`,
+        data: payload,
+        headers
+      }).then((response) => {
+        resolve(response.data)
+      }).catch((err) => {
+        reject(err)
       })
-    }
+    })
     /**
     * Get all payments
     * @param {object} filter - The filter object
     * @param {string} [filter.offset] - Offset for pagination.
     * @param {string} [filter.pageSize=20] - Per page items for pagination.
-    * @param {string} [filter.siteOperationId] - site_transaction_id 
+    * @param {string} [filter.siteOperationId] - site_transaction_id
     * @param {number} [filter.merchantId] - site id
     * @return {Promise}
     */
-    this.getPayments = (filter) => {
-      return new Promise((resolve, reject) => {
-        const {
-          offset = 0,
-          pageSize = 20,
-          siteOperationId,
-          merchantId
-        } = filter
-        let queryStr = ""
-        if (offset !== undefined) queryStr += `offset=${offset}&`
-        if (pageSize !== undefined) queryStr += `pageSize=${pageSize}&`
-        if (siteOperationId !== undefined) queryStr += `siteOperationId=${siteOperationId}&`
-        if (merchantId !== undefined) queryStr += `merchantId=${merchantId}&`
-        
-        axios({
-          method: "get",
-          url: `${endpoints["paymentsList"]}?${queryStr}`,
-          headers
-        }).then((response) => {
-          resolve(response.data)
-        }).catch((err) => {
-          reject(err)
-        })
+    this.getPayments = filter => new Promise((resolve, reject) => {
+      const {
+        offset = 0,
+        pageSize = 20,
+        siteOperationId,
+        merchantId
+      } = filter
+      let queryStr = ""
+      if (offset !== undefined) queryStr += `offset=${offset}&`
+      if (pageSize !== undefined) queryStr += `pageSize=${pageSize}&`
+      if (siteOperationId !== undefined) queryStr += `siteOperationId=${siteOperationId}&`
+      if (merchantId !== undefined) queryStr += `merchantId=${merchantId}&`
+
+      axios({
+        method: "get",
+        url: `${endpoints["paymentsList"]}?${queryStr}`,
+        headers
+      }).then((response) => {
+        resolve(response.data)
+      }).catch((err) => {
+        reject(err)
       })
-    }
+    })
     /**
     * Payment refund
     * @param {string} paymentId - Payment id / transaction id
     * @param {number} [amount] - In case of partial refund only and should not be greater than the actual amount
     * @return {Promise}
     */
-    this.paymentRefund = (paymentId, amount = null) => {
-      return new Promise((resolve, reject) => {
-        if (!(paymentId && typeof paymentId === "string")) reject(new Error("Invalid paymentId"))
-        const payload = {}
-        if (amount && typeof amount === "number" && amount > 0) payload.amount = amount
-        axios({
-          method: "post",
-          url: `${endpoints["paymentRefund"]["0"]}/${paymentId}${endpoints["paymentRefund"]["1"]}`,
-          headers,
-          data: payload
-        }).then((response) => {
-          resolve(response.data)
-        }).catch((err) => {
-          reject(err)
-        })
+    this.paymentRefund = (paymentId, amount = null) => new Promise((resolve, reject) => {
+      if (!(paymentId && typeof paymentId === "string")) reject(new Error("Invalid paymentId"))
+      const payload = {}
+      if (amount && typeof amount === "number" && amount > 0) payload.amount = amount
+      axios({
+        method: "post",
+        url: `${endpoints["paymentRefund"]["0"]}/${paymentId}${endpoints["paymentRefund"]["1"]}`,
+        headers,
+        data: payload
+      }).then((response) => {
+        resolve(response.data)
+      }).catch((err) => {
+        reject(err)
       })
-    }
+    })
   }
 }
